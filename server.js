774ea57cba185b5097a0dc23750253647f251882
Uros <Uros.Zivadinovic7@gmail.com>
2023-11-25 14:55:54 +0100

    Backend implemented

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
 const express = require("express");
+require("dotenv").config();
 
-const port = 5003;
+const port = process.env.PORT || 5003;
+const connectDB = require("./config/db");
+
+connectDB();
+  
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: "Positive Newsletter, a newsletter that only shares positive,uplifting news",
-    tag: "Technology",
-    username: "TonyStark",
-    date: "2022-01-02",
-  },
-  {
-    id: 2,
-    text: "Positive Newsletter, a newsletter that only shares positive,uplifting news",
-    tag: "Technology",
-    username: "TonySoprano",
-    date: "2022-02-02",
-  },
-];
+// Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcoem to the RandomIdeas API" });
+  res.json({ message: "Welcome to the RandomIdeas API" });
 });
 
-// Get all ideas
-app.get("/api/ideas", (req, res) => {
-  res.json({ success: true, data: ideas });
-});
-
-app.get("/api/ideas/:id", (req, res) => {
-  const idea = ideas.find(idea => idea.id === +req.params.id);
-
-  if (!idea) {
-    res.status(404).json({ success: false, error: "Resource not found" });
-  }
-
-  res.json({ success: true, data: idea });
-});
+const ideasRouter = require("./routes/ideas");
+app.use("/api/ideas", ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
